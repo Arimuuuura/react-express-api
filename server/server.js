30016e6afa330059ba3e7c1527e7092b1f5b22ce
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-05-13 16:53:54 +0900

    github api 呼び出し処理

--- server/server.js
+++ server/server.js
@@ -1,1 +1,27 @@
-console.log("Hello");
+const express = require("express");
+const app = express();
+const request = require("request");
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+	res.send("Hello from backend");
+});
+
+app.get("/api/github", (req, res) => {
+	request(options, (error, response, body) => {
+		res.json(body);
+	});
+});
+
+const options = {
+    url: 'https://api.github.com/users/arimuuuura',
+    method: 'GET',
+	headers: {
+		'User-Agent': 'request'
+	}
+}
+
+app.listen(PORT, () => {
+	console.log(`Listen port ${PORT}... http://localhost:${PORT}`);
+});
