5b411da59c3bb3fd5f7f291a81832cf2db9c5884
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-05-13 23:21:50 +0900

    client 側でデータ取得

--- README.md
+++ README.md
@@ -3,6 +3,24 @@
 ## Quick Start
 
 ```
-$ npm install
-$ npm run start
+# Install dependencies for server
+npm install
+
+# Install dependencies for client
+npm run client-install
+
+# Run the client & server with concurrently
+npm run dev
+
+# Server runs on http://localhost:5000 and client on http://localhost:3000
+```
+
+## develop
+
+```
+# Run the Express server only
+npm run server
+
+# Run the React client only
+npm run client
 ```
--- server/server.js
+++ server/server.js
@@ -1,26 +1,30 @@
 const express = require("express");
 const app = express();
 const request = require("request");
-
+const cors = require("cors");
 const PORT = 5000;
+
+app.use(cors())
 
 app.get("/", (req, res) => {
 	res.send("Hello from backend");
 });
 
-app.get("/api/github", (req, res) => {
-	request(options, (error, response, body) => {
-		res.json(body);
-	});
-});
-
 const options = {
-    url: 'https://api.github.com/users/arimuuuura',
-    method: 'GET',
+	url: 'https://api.github.com/users/arimuuuura',
+	method: 'GET',
+	json: true,
 	headers: {
 		'User-Agent': 'request'
 	}
 }
+
+const github = request(options, (error, response, body) => body);
+
+app.get("/api/github", (req, res) => {
+	const accountData = github.response.body;
+	res.json({accountData});
+});
 
 app.listen(PORT, () => {
 	console.log(`Backend server port ${PORT}... http://localhost:${PORT}`);
